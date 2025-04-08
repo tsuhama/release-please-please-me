fe1c00628e5f113d234bfe076a4e4e2fa6c439e2
tsuhama <39031636+tsuhama@users.noreply.github.com>
2025-04-08 22:17:00 +0200

    Min. linting and formatting  (#2)

--- src/core/version.ts
+++ src/core/version.ts
@@ -1,28 +1,28 @@
-import {Version} from "release-please/build/src/version";
+import { Version } from "release-please/build/src/version";
 
 export interface SemanticVersion {
-    readonly patch: number;
-    readonly minor: number;
-    readonly major: number;
+  readonly patch: number;
+  readonly minor: number;
+  readonly major: number;
 }
 
 export interface StableVersion {
-    readonly minor: number
-    readonly major: number
+  readonly minor: number;
+  readonly major: number;
 }
 
 export function toStableVersion(version: SemanticVersion): StableVersion {
-    return {
-        minor: version.minor,
-        major: version.major
-    };
+  return {
+    minor: version.minor,
+    major: version.major,
+  };
 }
 
 export function parseSemanticVersion(versionValue: string): SemanticVersion {
-    const version = Version.parse(versionValue);
-    return {
-        major: version.major,
-        minor: version.minor,
-        patch: version.patch
-    };
+  const version = Version.parse(versionValue);
+  return {
+    major: version.major,
+    minor: version.minor,
+    patch: version.patch,
+  };
 }
--- src/workflows/backport.ts
+++ src/workflows/backport.ts
@@ -1,17 +1,21 @@
 // backporting fix branch to all stable version branches by creating pull request against each of the stable version
-import {GitControl} from "../api/git";
-import {getStableVersionBranches, StableVersionMatcher} from "./maintenance";
+import { GitControl } from "../api/git";
+import { getStableVersionBranches, StableVersionMatcher } from "./maintenance";
 
 export async function backportFixBranch(
-    git: GitControl,
-    matcher: StableVersionMatcher,
-    fixBranchName: string
+  git: GitControl,
+  matcher: StableVersionMatcher,
+  fixBranchName: string,
 ) {
-    const stableVersionBranches = await getStableVersionBranches(git, matcher);
-    const fixBranch = await git.getBranch(fixBranchName);
-    for (const it of stableVersionBranches) {
-        console.info(`creating backport pull request from ${fixBranchName} against ${it.branch.name}.`);
-        const pr = await git.createPullRequest(fixBranch, it.branch);
-        console.info(`successfully created backport pull request ${pr.number} for stable version ${it.version}.`);
-    }
+  const stableVersionBranches = await getStableVersionBranches(git, matcher);
+  const fixBranch = await git.getBranch(fixBranchName);
+  for (const it of stableVersionBranches) {
+    console.info(
+      `creating backport pull request from ${fixBranchName} against ${it.branch.name}.`,
+    );
+    const pr = await git.createPullRequest(fixBranch, it.branch);
+    console.info(
+      `successfully created backport pull request ${pr.number} for stable version ${it.version}.`,
+    );
+  }
 }
